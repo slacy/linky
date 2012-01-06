0c249977255decc05cafbe70ec4b3b158000ea50
Steve Lacy <slacy@slacy.com>
2012-01-06 13:47:32 -0800

    Checkpoint

--- linky/build.py
+++ linky/build.py
@@ -1,6 +1,7 @@
 import os
 import markdown
 from jinja2 import Environment, FileSystemLoader, Template
+from nltk.util import bigrams, trigrams
 
 # Recursively find all text files
 # Process files into lists of words
@@ -43,30 +44,50 @@ def find_root(filepath):
         fullpath = dirname
     return None
 
+class Preprocessor(object):
+    def __init__(self):
+        self.bigram_map = {}
 
-def compile(filename):
-    print "Compile %s" % filename
-    root = find_root(filename)
-    print "Root is %s" % root
-    layout = find_layout(filename)
-    layout = layout.replace(root, '')
-    layout = layout.lstrip('/')
-    print "layout is %s" % layout
-    env = Environment(loader=FileSystemLoader(root))
-    out_file = open(filename.replace('.md', '.html'), 'w+')
-    in_file = open(filename)
-    html = markdown.markdown(in_file.read())
-    template = env.get_template(layout)
-    final = template.render(html=html)
-    out_file.write(final)
-    out_file.close()
-    in_file.close()
+    def process(self, filename):
+        content = open(filename).read()
+        words = content.split(' ')
+        grams = bigrams(words)
+        for gram in grams:
+            if gram not in self.bigram_map:
+                self.bigram_map[gram] = []
+            self.bigram_map[gram].append(filename)
+
+    def trim_bigrams(self):
+        for gram in self.bigram_map.keys():
+            if len(self.bigram_map[gram]) == 1:
+                del self.bigram_map[gram]
+
+    def compile(self, filename):
+        root = find_root(filename)
+        layout = find_layout(filename)
+        layout = layout.replace(root, '')
+        layout = layout.lstrip('/')
+        env = Environment(loader=FileSystemLoader(root))
+        out_file = open(filename.replace('.md', '.html'), 'w+')
+        in_file = open(filename)
+        html = markdown.markdown(in_file.read())
+        template = env.get_template(layout)
+        final = template.render(html=html)
+        out_file.write(final)
+        out_file.close()
+        in_file.close()
 
 
 def main():
     corpus = find_corpus(os.curdir, '.md')
+
+    pre = Preprocessor()
     for filename in corpus:
-        compile(filename)
+        pre.process(filename)
+    pre.trim_bigrams()
+
+    for filename in corpus:
+        pre.compile(filename)
 
 if __name__ == '__main__':
     main()
