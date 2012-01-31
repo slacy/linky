b2c0fdc5197fd05b2f93cead1efc256fe6ba4c67
Steve Lacy <slacy@slacy.com>
2012-01-31 11:22:22 -0800

    Linky simple supports markdown tag now

--- linky/simple.py
+++ linky/simple.py
@@ -1,6 +1,9 @@
 """yo gabba"""
 import os
 from jinja2 import Environment, FileSystemLoader
+from jinja2.ext import Extension
+from jinja2.nodes import CallBlock
+import markdown2
 
 # Recursively find all text files
 # Process files into lists of words
@@ -11,14 +14,42 @@ from jinja2 import Environment, FileSystemLoader
 # reprocess all documents, generating links for every N-gram
 
 
+class MarkdownExtension(Extension):
+    """A markdown tag for Jinja2"""
+    tags = set(['markdown'])
+
+    def __init__(self, environment):
+        super(MarkdownExtension, self).__init__(environment)
+        environment.extend(
+            markdowner=markdown2.Markdown()
+        )
+
+    def parse(self, parser):
+        lineno = parser.stream.next().lineno
+        body = parser.parse_statements(
+            ['name:endmarkdown'],
+            drop_needle=True
+        )
+        return CallBlock(
+            self.call_method('_markdown_support'),
+            [],
+            [],
+            body
+        ).set_lineno(lineno)
+
+    def _markdown_support(self, caller):
+        """WAT"""
+        return self.environment.markdowner.convert(caller()).strip()
+
+
 def find_corpus(root, suffix):
     """find_corpus"""
 
-    def visit(found, dirname, names):
+    def visit(_found, dirname, names):
         """visit"""
         new = [os.path.abspath(os.path.join(dirname, fn))
                for fn in names if fn.endswith(suffix)]
-        found += new
+        _found += new
 
     found_files = []
 
@@ -62,10 +93,7 @@ class Preprocessor(object):
         print "compiling %s" % filename
         root = find_root(filename)
         print "root is %s" % root
-        # layout = find_layout(filename)
-        # layout = layout.replace(root, '')
-        # layout = layout.lstrip('/')
-        env = Environment(loader=FileSystemLoader(root))
+        env = Environment(loader=FileSystemLoader(root), extensions=[MarkdownExtension])
         out_filename = filename.replace('.jinja2', '.html')
         out_file = open(out_filename, 'w+')
         # html = open(filename, 'r').read()
