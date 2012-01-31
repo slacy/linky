3d8a100c17699fa3bb1efc6cb84d9b6d03ef233b
Steve Lacy <slacy@slacy.com>
2012-01-31 12:37:12 -0800

    Better linky installer that goes into virtualenv bin directory

--- linky/simple.py
+++ linky/simple.py
@@ -1,9 +1,10 @@
 """yo gabba"""
 import os
+
 from jinja2 import Environment, FileSystemLoader
-from jinja2.ext import Extension
-from jinja2.nodes import CallBlock
-import markdown2
+
+from linky.markdown import MarkdownExtension
+
 
 # Recursively find all text files
 # Process files into lists of words
@@ -12,34 +13,6 @@ import markdown2
 # filter out bigrams and trigrams that start or end with a stopword
 # Create mapping from N-gram to list of documents that contain in
 # reprocess all documents, generating links for every N-gram
-
-
-class MarkdownExtension(Extension):
-    """A markdown tag for Jinja2"""
-    tags = set(['markdown'])
-
-    def __init__(self, environment):
-        super(MarkdownExtension, self).__init__(environment)
-        environment.extend(
-            markdowner=markdown2.Markdown()
-        )
-
-    def parse(self, parser):
-        lineno = parser.stream.next().lineno
-        body = parser.parse_statements(
-            ['name:endmarkdown'],
-            drop_needle=True
-        )
-        return CallBlock(
-            self.call_method('_markdown_support'),
-            [],
-            [],
-            body
-        ).set_lineno(lineno)
-
-    def _markdown_support(self, caller):
-        """WAT"""
-        return self.environment.markdowner.convert(caller()).strip()
 
 
 def find_corpus(root, suffix):
@@ -93,7 +66,8 @@ class Preprocessor(object):
         print "compiling %s" % filename
         root = find_root(filename)
         print "root is %s" % root
-        env = Environment(loader=FileSystemLoader(root), extensions=[MarkdownExtension])
+        env = Environment(loader=FileSystemLoader(root),
+                          extensions=[MarkdownExtension])
         out_filename = filename.replace('.jinja2', '.html')
         out_file = open(out_filename, 'w+')
         # html = open(filename, 'r').read()
